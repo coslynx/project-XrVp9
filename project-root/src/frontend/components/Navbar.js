e7b201dc27e0dbb5a39df7ec49fc38dec840f230
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-02 14:00:04 +0530

    Added: `project-root/src/frontend/components/Navbar.js`

---
+++ project-root/src/frontend/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <ul className="nav-list">
+                <li className="nav-item">Debadge</li>
+                <li className="nav-item">Test2</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
